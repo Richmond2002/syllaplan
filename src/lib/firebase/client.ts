416d2e0baaf15236e1478a04c6d5169bbf8317a4
Richmond Koomson <richmondkoomson6019@gmail.com>
2025-07-22 12:29:39 +0000

    File changes

---
+++ src/lib/firebase/client.ts
@@ -0,0 +1,16 @@
+// src/lib/firebase/client.ts
+import { initializeApp, getApps, getApp } from "firebase/app";
+
+const firebaseConfig = {
+  projectId: "courseforge-y5ymc",
+  appId: "1:114082963667:web:3bc06cf3634f0da03db426",
+  storageBucket: "courseforge-y5ymc.firebasestorage.app",
+  apiKey: "AIzaSyAmduNNtQiXZNK5FeCamHfgaMZYMRzkXKc",
+  authDomain: "courseforge-y5ymc.firebaseapp.com",
+  messagingSenderId: "114082963667",
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+export { app };
