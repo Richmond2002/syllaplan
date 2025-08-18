1c1655ffe182645e6562f409258786c08bbf20eb
Richmond Koomson <richmondkoomson6019@gmail.com>
2025-08-18 23:50:15 +0000

    now shift the card to the right side instead of left side

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -5,8 +5,8 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50 dark:bg-gray-900 p-4">
-      {/* This inner div is now flexed to the center by the parent */}
+    <div className="min-h-screen flex items-center justify-end bg-gray-50 dark:bg-gray-900 p-4 sm:p-8 lg:p-16">
+      {/* This inner div is now flexed to the right by the parent */}
       {children}
     </div>
   );
