eacb544f24c91e66cce374c59cb3aa78928f4606
Richmond Koomson <richmondkoomson6019@gmail.com>
2025-08-19 16:23:22 +0000

    landing page, loginpage, signuppage are all shifted to the left. i dont

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -5,8 +5,7 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen flex items-center justify-end bg-background p-4 sm:p-8 lg:p-16">
-      {/* This inner div is now flexed to the right by the parent */}
+    <div className="min-h-screen flex items-center justify-center bg-background p-4">
       {children}
     </div>
   );
